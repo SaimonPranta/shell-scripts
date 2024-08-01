7a8d41bdc8d5529a82031ba15a07b96d0afcd989
Saimon Pranta <101664170+SaimonPranta@users.noreply.github.com>
2024-08-01 14:49:02 +0600

    Added run-server.sh scripts file

--- run-server.sh
+++ run-server.sh
@@ -1,13 +1,11 @@
 !/bin/bash
 
-
 # Define the directory path
 directory_path="/var/micple.com/panel/frontend"
 
 # Get the folder names and store them in an array
 folder_array=($(ls -d $directory_path/*/ | xargs -n 1 basename))
  
-
 for folder in "${folder_array[@]}"; do
    
   if [[ $folder != *backup* ]];
